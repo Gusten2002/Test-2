59605d2bcdddedaea658b382ae28f6911dc47254
August <august.agren@elev.ga.ntig.se>
2020-09-01 10:51:28 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // File.WriteAllText("Words.txt", "Thing");
+            // String s = File.ReadAllText("words.txt");
+
+            // Console.WriteLine(s);
+
+            // string [] words = {"Hat", "Coat", "Pants"};
+
+            // File.WriteAllLines("words.txt", words);
+
+            if (File.Exists("words.txt"))
+            {
+            string [] words = File.ReadAllLines("words.txt");
+
+            Console.WriteLine(words[0]);
+
+            Console.ReadLine();
+
+            }
+
+        }
+    }
+}
